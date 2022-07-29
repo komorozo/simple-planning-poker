92370da5c60652a0979cf55849358290a68569ba
Lukasz Komorowski <komorozo87@gmail.com>
2022-07-29 11:04:44 +0200

    simple validation added

--- src/components/Main/CreateGamePanel.js
+++ src/components/Main/CreateGamePanel.js
@@ -4,11 +4,22 @@ import './CreateGamePanel.css';
 
 const CreateGamePanel = () => {
     const [inputValue, setInputValue] = useState('');
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(inputValue)
+        if(!inputValue) {
+            setShowErrorMessage(true)
+            setErrorMessage('Please put name of new game')
+            return
+        } else if (inputValue.length < 3) {
+            setErrorMessage('Name of table sholud be at least 3 signs')
+            setShowErrorMessage(true)
+            return
+        }
         navigate(inputValue)
     }
     const handleInputValue = (e) => {
@@ -19,7 +30,8 @@ const CreateGamePanel = () => {
         <div className="new-game-panel">
             <form id="create-new-game-form" onSubmit={handleSubmit}>
                 <label htmlFor="game-name">Put name of the room</label>
-                <input type='text' id='game-name' onChange={(e) => handleInputValue(e)} value={inputValue}></input>
+                {showErrorMessage && <span className='errorMsg'> {errorMessage}</span>}
+                <input type='text' id='game-name' onChange={(e) => handleInputValue(e)} value={inputValue} className={showErrorMessage ? 'error' : null}></input>
                 <button type='submit'>Create New Game</button>
             </form>
         </div>
